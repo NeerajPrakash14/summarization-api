7560fce0e4c6664489b8f08f5ed09eaa8a5809fc
Neeraj Prakash <neerajprakashreddy@gmail.com>
2020-05-04 01:12:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ app=Flask(__name__)
 
 @app.route("/summary/",methods=['POST','GET'])
 def return_summary():
-    article_text='Either you love it or hate it, but in the age of Microservice and REST API, you can not ignore JavaScript. JavaScript was once upon a time used only in client side(browser), but node js (execution engine/run time/web server) have made possible to run javascript on server side. JavaScript is everywhere – on Desktop/Server/Mobile.You can create mobile web app with javascript and html5, which has lot of advantages like save licensing cost $99 yearly to pay Apple for making IOS apps and you don’t have to purchase MAC laptop to make your IOS app(Apple’s app can only be made in MAC). JavaScript has stormed the web technology and nowadays small software ventures to fortune 500, all are using node js for web apps. Recently wordpress.com has rewritten its dashboard in javascript, paypal also chose to rewrite some of its components in java script. Be it google/twitter/facebook, javascript is important for everyone. It is used in applications like single page applications, Geolocation APIs, net advertisements etc. However JavaScript is quirky/dynamic/scripting/ functional oriented language, and it has its own idiosyncrasies. It is not scalable, it is good for some 3000 line of code but for a bigger app, it becomes difficult to manage ,read and debug. Also not everyone is very much familiar to JavaScript.'
+    #article_text='Either you love it or hate it, but in the age of Microservice and REST API, you can not ignore JavaScript. JavaScript was once upon a time used only in client side(browser), but node js (execution engine/run time/web server) have made possible to run javascript on server side. JavaScript is everywhere – on Desktop/Server/Mobile.You can create mobile web app with javascript and html5, which has lot of advantages like save licensing cost $99 yearly to pay Apple for making IOS apps and you don’t have to purchase MAC laptop to make your IOS app(Apple’s app can only be made in MAC). JavaScript has stormed the web technology and nowadays small software ventures to fortune 500, all are using node js for web apps. Recently wordpress.com has rewritten its dashboard in javascript, paypal also chose to rewrite some of its components in java script. Be it google/twitter/facebook, javascript is important for everyone. It is used in applications like single page applications, Geolocation APIs, net advertisements etc. However JavaScript is quirky/dynamic/scripting/ functional oriented language, and it has its own idiosyncrasies. It is not scalable, it is good for some 3000 line of code but for a bigger app, it becomes difficult to manage ,read and debug. Also not everyone is very much familiar to JavaScript.'
     article_text=request.args.get('document')
     # Removing Square Brackets and Extra Spaces
-    #article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-    #article_text = re.sub(r'\s+', ' ', article_text)
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
 
     # Removing special characters and digits
-    #formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-    #formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-    formatted_article_text=article_text
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    #formatted_article_text=article_text
     sentence_list = nltk.sent_tokenize(article_text)
 
     stopwords = nltk.corpus.stopwords.words('english')
@@ -59,10 +59,13 @@ def return_summary():
     summary_sentences =summary_sentences[0:4]
     #summary = ' '.join(summary_sentences)
     ordered_summary=''
+    '''
     for sentence in sentence_list:
         if(sentence in summary_sentences):
             ordered_summary+=sentence
-
+'''
+    for i in sentence_scores:
+        ordered_summary+=i
     return jsonify({'summary':ordered_summary})
     
 
